7cc123a21601de8cbbfa3a04b12fce6fd8e9bcfc
MiquelBlanch <info@miquelblanch.com>
2023-05-21 23:40:50 +0200

    seventh

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import streamlit as st
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 def main():
-    st.title("Formulario de Registcccro")
+    st.title("Formulario de Registro")
 
     # Pregunta el nombre
     nombre = st.text_input("Ingrese su nombre")
