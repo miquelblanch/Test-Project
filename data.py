269bf0b17c9938a7c779114bdea4f4ad7d67be61
MiquelBlanch <info@miquelblanch.com>
2023-05-21 18:07:07 +0200

    New

--- data.py
+++ data.py
@@ -1,9 +1,10 @@
-import numpy as np
-import pandas as pd 
-import matplotlib.pyplot as plt
+import streamlit as st
+from streamlit_jina import jina
+st.set_page_config(page_title="Jina Text Search",)
 
-df = pd.DataFrame({"A":[1,2,3], "B":[4,5,6]})
+endpoint = "http://0.0.0.0:45678/api/search"
 
-df2 = pd.DataFrame({"A":[8,9,10], "B":[4,5,6]})
+st.title("Jina Text Search")
+st.markdown("You can run our [Wikipedia search example](https://github.com/jina-ai/examples/tree/master/wikipedia-sentences) to test out this search")
 
-print(df2)
+jina.text_search(endpoint=endpoint)
