6eaadec31fbadd4eff25fa44ff906e86c24c0e23
MiquelBlanch <info@miquelblanch.com>
2023-05-21 18:44:28 +0200

    First

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st 
+
+## para arrancar, escribir por terminal "streamlit run main.py"
+## Local URL: http://localhost:8501
+##  Network URL: http://192.168.4.36:8501
+
+
+st.title("Hi I am Miquel")
+st.subheader("fhiurfhfuirt")
+st.header("djrbrh")
+st.text("hweuybevr")
+st.markdown("---")
+st.caption("fjriu")
+genre = st.radio(
+    "What\'s your favorite movie genre",
+    ('Comedy', 'Drama', 'Documentary'))
